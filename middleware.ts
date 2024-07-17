ef126eff0dfc106553b3c1131ac60d08a86f6e79
Rinka <102121042@nitt.edu>
2024-07-17 10:14:32 +0530

    third commit

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,41 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+// import { authMiddleware } from "@clerk/nextjs";
  
-export default authMiddleware({
-  publicRoutes: [
-    '/',
-    '/events/:id',
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+// export default authMiddleware({
+//   publicRoutes: [
+//     '/',
+//     '/events/:id',
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+ 
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+ 
+import {
+  clerkMiddleware,
+  createRouteMatcher
+} from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher([
+  '/',
+  '/events/:id',
+  '/api/webhook/clerk',
+  '/api/webhook/stripe',
+  '/api/uploadthing'
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
