fbc01eaea2f0f1e22be040547df74347b482073b
Rinka <102121042@nitt.edu>
2024-07-17 11:26:57 +0530

    fifth

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,33 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
  
-export default clerkMiddleware({
-  publicRoutes: [
-    '/',
+// export default clerkMiddleware({
+//   publicRoutes: [
+    
+//   ],
+//   ignoredRoutes: [
+//     '/api/webhook/clerk',
+//     '/api/webhook/stripe',
+//     '/api/uploadthing'
+//   ]
+// });
+ 
+// export const config = {
+//   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+// };
+
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isProtectedRoute = createRouteMatcher(['/',
     '/events/:id',
     '/api/webhook/clerk',
     '/api/webhook/stripe',
-    '/api/uploadthing'
-  ],
-  ignoredRoutes: [
-    '/api/webhook/clerk',
-    '/api/webhook/stripe',
-    '/api/uploadthing'
-  ]
+    '/api/uploadthing']);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
- 
+
 export const config = {
-  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/"],
 };
 
